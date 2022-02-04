2dbd721bf1d2b58f0dfb1b8bd53deff265cbe837
Khrononian <Alv.Barnes@gmail.com>
2022-02-04 14:04:58 -0500

    Refactored incorrect tests

--- src/Functionality/Ship.js
+++ src/Functionality/Ship.js
@@ -1,32 +1,35 @@
-const CreateBattleShips = (shipName, length) => {
-    return {
-        shipName,
-        length, 
-        sunk: false,
-        location: [], // USE PLACEMENT OF SHIP TO CALCULATE LOCATION, ALSO CHANGE LOCATION TO PARAMETER
-        hit(num) {
-            const numIndex = this.location.indexOf(num)
+import { enemyWaters, allyWaters } from './Dom'
+import GameBoard from './GameBoard'
 
-            if (numIndex && num) {
-                this.location.push(num)
-                this.location.splice(numIndex, 1, 'X')
-            }
+const CreateShips = (shipName, length) => {
+    let sunk = false;
+
+    const hitCoordinates = [];
+
+    const hit = num => {
+        if (hitCoordinates.indexOf(num) == -1) {
+            hitCoordinates.push(num)
             
-            return this.location
-        },
-        isSunk() {
-            if (this.location.every(x => x === 'X') && this.location.length === this.length) return this.sunk = true;
-            else return this.sunk = false;
-        },
+            return hitCoordinates
+        } 
     }
+
+    const isSunk = (length) => {
+        if (hitCoordinates.length == length) {
+            return sunk = true
+        } else return sunk = false
+    }
+
+    return { hit, isSunk }
 }
-const ship = CreateBattleShips('Carrier', 5, false);
+const ship = CreateShips('Carrier', 5, false);
+const accessShips = CreateShips()
 
-const carrier = CreateBattleShips('Carrier', 5, false);
-const battleShip = CreateBattleShips('Battle Ship', 4, false);
-const destroyer = CreateBattleShips('Destroyer', 3, false);
-const submarine = CreateBattleShips('Submarine', 3, false);
-const patrolBoat = CreateBattleShips('Patrol Boat', 2, false);
+const carrier = CreateShips('Carrier', 5, false);
+const battleShip = CreateShips('Battle Ship', 4, false);
+const destroyer = CreateShips('Destroyer', 3, false);
+const submarine = CreateShips('Submarine', 3, false);
+const patrolBoat = CreateShips('Patrol Boat', 2, false);
 
-export default CreateBattleShips
-export { ship, carrier, battleShip, destroyer, submarine, patrolBoat }
+export default CreateShips
+export { ship, carrier, battleShip, destroyer, submarine, patrolBoat, accessShips }
