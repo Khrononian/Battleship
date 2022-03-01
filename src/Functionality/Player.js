950b347e96d09ec4f10ccf1b2be7ae59c244420f
Khrononian <Alv.Barnes@gmail.com>
2022-03-01 17:42:23 -0500

    Functionality for player and computer

--- src/Functionality/Player.js
+++ src/Functionality/Player.js
@@ -1,19 +1,24 @@
-import { getGridPosition, allyWaters } from './Dom'
 import GameBoard from './GameBoard'
 
 const Player = () => {
-    const getGame = GameBoard()
+    const human = GameBoard();
+    const computer = GameBoard();
+    const playerAttacks = []
     
-    const gamePlayer = (choosePlayer) => {
-            
+    const gamePlayer = (player, coordinates) => {
+        const attackHuman = human.receiveAttack(coordinates);
+        const attackComputer = computer.receiveAttack(coordinates);
+
+        if (player == 'Human') return attackHuman
+        else return attackComputer
     }
         
-     // USE BOTH, PLAYER AND COMPUTER GAMEBOARDS, TO RECORD EACH SHOT ON THEIR BOARD
+    // USE BOTH, PLAYER AND COMPUTER GAMEBOARDS, TO RECORD EACH SHOT ON THEIR BOARD
     // MAKE SURE CHECK IF A POSITION IS POSSIBLE OR NOT
     // USE RANDOM FOR COMPUTER TO MAKE RANDOM CHOICES
             
             
-    return { gamePlayer }
+    return { gamePlayer, human, computer }
 }
 
 export default Player
