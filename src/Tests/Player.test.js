35f4da04254304be987b6b6c6b006c0ea1e4437b
Khrononian <Alv.Barnes@gmail.com>
2022-03-04 03:50:57 -0500

    Added clear board test

--- src/Tests/Player.test.js
+++ src/Tests/Player.test.js
@@ -9,7 +9,7 @@ beforeEach(() => {
 describe('Players', () => {
     test('Attack enemy', () => {
         player.human.placeShips('Carrier', 5, [0, 4])
-        player.gamePlayer('Human', [0, 4])
+        player.attackPlayer('Human', [0, 4])
 
         expect(player.human.board[4]).toEqual([0, 4, {name: 'Carrier', shot: true}])
     })
@@ -18,6 +18,13 @@ describe('Players', () => {
         player.human.placeShips('Carrier', 5, [0, 2])
         player.human.receiveAttack([0, 2])
 
-        expect(player.gamePlayer('Human', [0, 2])).toBe('ALREADY SHOT')
+        expect(player.attackPlayer('Human', [0, 2])).toBe('ALREADY SHOT')
+    })
+
+    test('Clear boards', () => {
+        player.human.placeShips('Carrier', 5, [0, 3], 'Horizontal')
+
+        expect(player.restartPlayers('Human')[3]).toEqual([0, 3])
+        expect(player.restartPlayers('Computer')).toEqual(player.restartPlayers('Computer'))
     })
 })
