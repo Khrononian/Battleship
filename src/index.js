ba370c70d6cea462fb6bb20798a953bd253b2312
Khrononian <Alv.Barnes@gmail.com>
2022-04-11 03:20:05 -0400

    New load event listener added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-import rice from './Functionality/Dom'
+import { 
+    enemyWaters, allyWaters, restartBtn, hoverGridCell, 
+    hoverOutGridCell, hoverOutShipPlacements, hoverShipPlacements, 
+    shipZone, shipBlocks, rotateBtn, rotateShip, restartBoard
+} from './Functionality/Dom'
+import { getGridPosition, getElementIndex, clickGridCell} from './Functionality/AttackBoard';
+import { clickShipPlacement } from './Functionality/PlaceShips';
 
-console.log('Food', rice)
+const loadGridBlocks = () => {
+    for (let i = 0; i < 100; i++) {
+        const enemyDivs = document.createElement('div');
+        const allyDivs = document.createElement('div');
+        const shipDivs = document.createElement('div');
+
+        enemyDivs.classList.add('grid-cell')
+        enemyDivs.classList.add('grid-enemy')
+
+        enemyDivs.addEventListener('mouseover', hoverGridCell)
+        enemyDivs.addEventListener('click', clickGridCell)
+        enemyDivs.addEventListener('mouseout', hoverOutGridCell)
+        shipDivs.addEventListener('mouseover', hoverShipPlacements)
+        shipDivs.addEventListener('mouseout', hoverOutShipPlacements)
+        shipDivs.addEventListener('click', clickShipPlacement);
+        
+        enemyWaters.append(enemyDivs)
+        allyDivs.classList.add('grid-ally');
+        allyWaters.append(allyDivs);
+        enemyDivs.style.cursor = 'pointer'
+        shipZone.append(shipDivs)
+        shipDivs.classList.add('grid-ships')
+        // shipBlocks.style.visibility = 'unset'
+        shipZone.append(shipBlocks)
+
+        enemyDivs.dataset.row = Number(getGridPosition(enemyWaters, getElementIndex(enemyDivs)).row);
+        enemyDivs.dataset.column = Number(getGridPosition(enemyWaters, getElementIndex(enemyDivs)).column);
+        allyDivs.dataset.row = Number(getGridPosition(allyWaters, getElementIndex(allyDivs)).row);
+        allyDivs.dataset.column = Number(getGridPosition(allyWaters, getElementIndex(allyDivs)).column);
+        shipDivs.dataset.row = Number(getGridPosition(shipZone, getElementIndex(shipDivs)).row);
+        shipDivs.dataset.column = Number(getGridPosition(shipZone, getElementIndex(shipDivs)).column);
+        allyDivs.dataset.shot = false;
+    }
+    restartBtn.addEventListener('click', restartBoard)
+    rotateBtn.addEventListener('click', rotateShip)
+}
+
+window.addEventListener('load', loadGridBlocks)
+
+export default loadGridBlocks
